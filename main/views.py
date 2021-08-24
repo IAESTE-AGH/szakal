9d6fa8662d16d801de48f7ef95b7542e555203ae
SzumiecM <milosz.sz.m@gmail.com>
2021-08-24 15:06:29 -0700

    add simple form to creating user

--- main/views.py
+++ main/views.py
@@ -1,10 +1,32 @@
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from .forms import CreateUserForm
 
 
 class CreateUser(CreateView):
     model = User
-    fields = ['username']
+    template_name = 'auth/user_form.html'
+    fields = '__all__'
+    success_url = '/user'
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+    # def get(self, request, *args, **kwargs):
+    #     context = {'form': CreateUserForm()}
+    #     return render(request, 'auth/user_form.html', context)
+    #
+    # def post(self, request, *args, **kwargs):
+    #     form = CreateUserForm(request.POST)
+    #     if form.is_valid():
+    #         user = form.save()
+    #         user.save()
+    #         return HttpResponseRedirect(reverse_lazy('users:detail', args=[user.id]))
+    #     return render(request, 'create_user.html', {'form': form})
 
 
 def industry(request):
