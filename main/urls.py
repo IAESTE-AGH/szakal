d9aafea50307fe1121b9cfcf95128e15da0175ad
SzumiecM <milosz.sz.m@gmail.com>
2021-10-10 14:19:21 -0700

    add todo to consider

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
 
     path('add/<str:object_to_add>/', staff_member_required(views.AddObjectView.as_view()), name='add'),
 
+    # todo consider structure: <model>/my | <model>/all etc
     path('my_companies', views.MyCompanies.as_view(), name='my_companies'),
     path('all_companies', views.AllCompanies.as_view(), name='all_companies'),
     path('taken_companies', views.TakenCompanies.as_view(), name='taken_companies'),
