fcb4b3228ce900ac6bf1397fb6d330d07439fdef
Szymon Fugas <sfugas@virtuslab.com>
2021-12-28 23:19:23 +0100

    Whip up a script to create .env file for docker compose with required env variables

---
+++ setup_env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo DEBUG=0 >> .env
+
+echo SECRET_KEY=$SECRET_KEY >> .env
+echo SQL_DATABASE=$SQL_DATABASE >> .env
+echo SQL_USER=$SQL_USER >> .env
+echo SQL_PASSWORD=$SQL_PASSWORD >> .env
+echo SQL_HOST=$SQL_HOST >> .env
+echo SQL_PORT=$SQL_PORT >> .env
