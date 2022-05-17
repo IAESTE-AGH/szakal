10f7e633c5bffeb167317942fcee1cc1e8e9b1fb
szymon0w <odiszym123>
2022-05-17 12:57:32 -0700

    wyszukiwanie po nazwie

---
+++ main/helpers/filtering_algorithm.py
@@ -0,0 +1,38 @@
+def build_array(string1, string2):
+    M = []
+    for i in range(len(string1) + 1):
+        for j in range(len(string2) + 1):
+            if j == 0:
+                M.append([i])
+            elif i == 0:
+                M[i].append(j)
+            else:
+                M[i].append(0)
+    for i in range(1, len(string1) + 1):
+        for j in range(1, len(string2) + 1):
+            if string1[i - 1] == string2[j - 1]:
+                M[i][j] = M[i - 1][j - 1]
+            else:
+                M[i][j] = min(M[i - 1][j], M[i][j - 1], M[i - 1][j - 1]) + 1
+    return M
+
+
+def levenschtein_distance(string1, string2):
+    return build_array(string1, string2)[len(string1)][len(string2)]
+
+
+def filter_by_word(name, A):
+    print(name)
+    res = []
+    for i in range(10):
+        res.append([])
+    for row in A:
+        best = 9
+        for word in row.name.split():
+            best = min(levenschtein_distance(name, word), best)
+        res[best].append(row)
+    result = []
+    for i in range(len(res)):
+        for j in range(len(res[i])):
+            result.append(res[i][j])
+    return result
